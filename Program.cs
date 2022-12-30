7bf3b30da45685abf08f4d4d2ec597f28875571a
Nastya <anastasiya.komova@mail.ru>
2022-12-30 23:34:55 +0300

    Задача 66

--- Program.cs
+++ Program.cs
@@ -14,8 +14,24 @@ Console.Write("1.");
 */
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-// M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
+
+/*
+
+int SumNtoM(int m, int n)
+{
+    if(m < n) return SumNtoM(m,n-1)+n;
+    if(m > n) return SumNtoM(n,m-1)+m;
+    else return m;
+}
+
+Console.Write("Input a namber M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a namber N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"M = {m}; N = {n} -> {SumNtoM(m, n)}");
+*/
+
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
