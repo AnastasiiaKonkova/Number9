9cd06c1e52780e44be9ec63cfda6bcff21d2167b
Nastya <anastasiya.komova@mail.ru>
2022-12-31 13:57:16 +0300

    Задача 68

--- Program.cs
+++ Program.cs
@@ -33,5 +33,19 @@ Console.Write($"M = {m}; N = {n} -> {SumNtoM(m, n)}");
 */
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-// m = 2, n = 3 -> A(m,n) = 9
-// m = 3, n = 2 -> A(m,n) = 29
+
+/*
+int functionAckerman(int m,int n)
+{
+    if( m == 0 ) return (n+1);
+    else if( n == 0 ) return functionAckerman(m-1,1);
+    else return functionAckerman(m-1,functionAckerman(m,n-1));
+}
+
+Console.Write("Input a namber M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a namber N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"m = {m}, n = {n} -> A(m,n) = {functionAckerman(m, n)}");
+*/
